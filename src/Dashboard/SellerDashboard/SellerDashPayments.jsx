1e0e65622c0437d0c4d252ce856ed2a0822a47a9
Polash <ahmedpolash732@gmail.com>
2024-02-11 05:24:05 -0600

    payment route are completed

--- src/Dashboard/SellerDashboard/SellerDashPayments.jsx
+++ src/Dashboard/SellerDashboard/SellerDashPayments.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import DashHeaders from "../../Components/DashHeaders";
 import { BsCurrencyDollar } from "react-icons/bs";
 
-
 const SellerDashPayments = () => {
   return (
     <div className="px-2 md:px-3 lg:px-4">
@@ -49,6 +48,100 @@ const SellerDashPayments = () => {
               <BsCurrencyDollar className="text-[#7367f0] shadow-lg" />
             </div>
           </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="flex flex-wrap items-center flex-col lg:flex-row gap-4 md:gap-5 lg:gap-5">
+          {/* Box one start here */}
+          <div className="bg-white px-4 py-3 rounded-md w-full lg:w-1/2 h-[67vh]">
+            <h2 className="text-lg text-slate-700 mb-1 font-medium">
+              Send withdrawal Request
+            </h2>
+            <div className="py-1">
+              <form>
+                <div className="flex gap-3 flex-wrap">
+                  <input
+                    required
+                    min="0"
+                    type="number"
+                    className="px-3 w-full md:w-[79%] py-2 focus:border-indigo-500 outline-none bg-slate-100 border border-slate-700 rounded-md text-slate-700"
+                    name="amount"
+                  />
+                  <button className="bg-indigo-500  hover:shadow-indigo-500/50 hover:shadow-lg text-white rounded-md w-full md:w-24 lg:w-24 px-6 py-2 text-sm ">
+                    Submit
+                  </button>
+                  {/* <button
+                    disabled={loader}
+                    className="bg-indigo-500 hover:shadow-indigo-500/50 hover:shadow-lg text-white rounded-sm px-4 py-2 text-sm "
+                  >
+                    {loader ? "loading.." : "Submit"}
+                  </button> */}
+                </div>
+              </form>
+            </div>
+
+            <div className="py-2">
+              <h1 className="text-slate-700 mb-2 font-semibold">Pending Request</h1>
+
+              <div className="py-1">
+                <table className="overflow-x-auto w-full">
+                  <thead className="w-full text-[15px] font-normal text-slate-800 rounded-md bg-slate-200">
+                    <tr>
+                      <th>No</th>
+                      <th>Amount</th>
+                      <th>Status</th>
+                      <th>Date</th>
+                    </tr>
+                  </thead>
+                  <tbody className="w-full text-center">
+                    {[1, 2, 3, 4].map((p, i) => (
+                      <tr
+                        className="border-b text-slate-600 border-slate-200 bg-slate-50"
+                        key={i}
+                      >
+                        <td className="py-2">{i + 1}</td>
+                        <td className="py-2">$450</td>
+                        <td className="">
+                          <span className="text-green-500 rounded-full font-semibold text-sm px-2">
+                            Pending
+                          </span>
+                        </td>
+                        <td className="py-2">02-02-2024</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          {/* Box one start here */}
+
+          {/* box two start here */}
+
+          <div className="lg:flex-1 px-4 py-3 rounded-md bg-white w-full lg:w-1/2 h-[67vh]">
+            <h2 className="text-lg text-slate-700 font-medium">
+              Success Withdraw
+            </h2>
+            <div className="py-2">
+              <div className="py-1">
+                <table className="overflow-x-auto w-full">
+                  <thead className="w-full text-[15px]  font-normal text-slate-800 rounded-md bg-slate-200">
+                    <tr className="">
+                      <th>Id</th>
+                      <th>Amount</th>
+                      <th>Status</th>
+                      <th>Date</th>
+                    </tr>
+                  </thead>
+                  <tbody className="w-full text-center">
+                    
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          {/* box two start here */}
         </div>
       </section>
     </div>
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -7,13 +7,19 @@ import { BsCartCheck, BsCartCheckFill, BsTwitterX } from "react-icons/bs";
 import { MdKeyboardArrowDown, MdOutlineMarkEmailUnread } from "react-icons/md";
 import {
   FaGithub,
+  FaHome,
   FaLinkedin,
   FaList,
   FaRegUser,
   FaRegUserCircle,
   FaSearch,
 } from "react-icons/fa";
-import { FaAngleDown, FaArrowRightArrowLeft, FaH } from "react-icons/fa6";
+import {
+  FaAngleDown,
+  FaArrowRightArrowLeft,
+  FaCartShopping,
+  FaH,
+} from "react-icons/fa6";
 import "../../Responsive/Responsive.css";
 
 import { TiSocialFacebook } from "react-icons/ti";
@@ -92,7 +98,7 @@ const Navbar = () => {
 
       {/* Sub Menu */}
 
-      <div className="px-5 lg:px-16">
+      <div className="px-5 hidden lg:block lg:px-16">
         <div className=" border-b">
           <div className="flex py-5 justify-between items-center">
             <div className="block lg:hidden">
@@ -137,7 +143,7 @@ const Navbar = () => {
               </div>
             </div>
             <div className="hidden lg:block">
-              {user  ? (
+              {user ? (
                 <>
                   {token?.role === "user" && (
                     <Link to="/dashboard/my-dashboard">
@@ -205,95 +211,7 @@ const Navbar = () => {
             </div>
 
             {/* Mobile menu */}
-            <div
-              className={`${
-                open
-                  ? "translate-x-0 opacity-100"
-                  : "-translate-x-full opacity-0"
-              } lg:hidden transition-all duration-500 fixed  z-[99999] border border-gray-200 shadow-lg backdrop-blur-sm transform h-full sm-device w-[360px] md:w-[390px] bg-white text-black  top-0 left-0`}
-            >
-              <div className="flex justify-between">
-                <button
-                  className="px-4 relative text-[#FE2424] py-2 text-3xl font-semibold flex right-0 mt-2"
-                  onClick={() => setOpen(false)}
-                >
-                  <FaHeart />
-                </button>
 
-                <div className="absolute left-8 top-2 bg-[#3bc177] h-6 text-white w-6 text-center rounded-full">
-                  0
-                </div>
-
-                <button
-                  className="px-4 text-[#FE2424] py-2 text-3xl font-semibold flex absolute right-0 mt-2"
-                  onClick={() => setOpen(false)}
-                >
-                  <RxCross1 />
-                </button>
-              </div>
-
-              {/* Login registration */}
-              <div className="flex justify-center items-center gap-4">
-                {user && token ? (
-                  <div className=" cursor-pointer gap-2">
-                    <span className="text-3xl flex items-center justify-center text-red-500">
-                      {token?.role === "user" && (
-                        <Link to="/dashboard/my-dashboard">
-                          {" "}
-                          <FaRegUserCircle />
-                        </Link>
-                      )}
-                      {token?.role === "seller" && (
-                        <Link to="/dashboard/seller-dashboard">
-                          {" "}
-                          <FaRegUserCircle />
-                        </Link>
-                      )}
-                      {token?.role === "admin" && (
-                        <Link to="/dashboard/admin-dashboard">
-                          {" "}
-                          <FaRegUserCircle />
-                        </Link>
-                      )}
-                    </span>
-                    <p className="font-semibold text-[18px]">
-                      {user.displayName}
-                    </p>
-                  </div>
-                ) : (
-                  <>
-                    <FaRegUser className="hover:text-red-400 font-semibold duration-300" />
-
-                    <Link
-                      to="/login"
-                      className="hover:text-red-400 font-semibold duration-300"
-                    >
-                      Login
-                    </Link>
-                    <div className="border-r-2  border-gray-300 h-5 flex justify-center items-center mt-1"></div>
-                    <Link
-                      to="/register"
-                      className="hover:text-red-400 font-semibold duration-300"
-                    >
-                      Register
-                    </Link>
-                  </>
-                )}
-              </div>
-
-              <ul className="flex uppercase flex-col p-5 gap-5 text-[18px]">
-                {navList}
-              </ul>
-
-              <div className="flex justify-center items-center mx-a">
-                <Link
-                  to="/sellerRegistration"
-                  className="flex gap-1 ml-2  border rounded-full cursor-pointer hover:border-red-500 duration-300 text-[14px] font-semibold py-3 items-center  border-gray-400 px-4 hover:bg-transparent hover:text-black  bg-black text-white"
-                >
-                  <FaRegUser /> Seller Registration
-                </Link>
-              </div>
-            </div>
             {/* Mobile menu */}
 
             {/* Main menu */}
@@ -450,22 +368,28 @@ const Navbar = () => {
       {/* Search menu */}
 
       {/* bottom nav */}
-      {/* <div className="fixed block lg:hidden z-50 bottom-0 bg-slate-300 w-full">
+      <div className="fixed block lg:hidden z-50 bottom-0 bg-slate-300 w-full">
         <div>
-          <ul className="flex items-center justify-between  p-2">
+          <ul className="flex items-center justify-between p-2">
             <li>
-              <IoMdMenu
-                className="text-3xl cursor-pointer lg:hidden "
-                onClick={toggleMenu}
-              />
+              <FaHome />
             </li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
+            <li>
+              <FaHome />
+            </li>
+            <li className="bottom-6 relative bg-green-500 h-12 w-12 rounded-full ">
+              <FaCartShopping className="mx-auto mt-4 text-xl" />
+            </li>
+            <li>
+              <FaHome />
+            </li>
+
+            <li>
+              <FaHome />
+            </li>
           </ul>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
