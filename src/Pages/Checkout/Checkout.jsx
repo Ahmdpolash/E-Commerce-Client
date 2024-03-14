8e126e93da9283c0fcac58e002df746b39d48996
Polash <ahmedpolash732@gmail.com>
2024-03-14 04:44:19 -0600

    featured section and checkout page customize some design

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -4,19 +4,9 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Checkout = () => {
   const navigate = useNavigate();
-  const {
-    state: { products, price, shipping_fee, items },
-  } = useLocation();
+
   const [res, setRes] = useState(false);
-  const [state, setState] = useState({
-    name: "",
-    address: "",
-    phone: "",
-    post: "",
-    province: "",
-    city: "",
-    area: "",
-  });
+  const [state, setState] = useState();
   const inputHandle = (e) => {
     setState({
       ...state,
@@ -49,7 +39,6 @@ const Checkout = () => {
                           <label htmlFor="name">Name</label>
                           <input
                             onChange={inputHandle}
-                            value={state.name}
                             type="text"
                             className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
                             name="name"
@@ -61,7 +50,6 @@ const Checkout = () => {
                           <label htmlFor="address">Address</label>
                           <input
                             onChange={inputHandle}
-                            value={state.address}
                             type="text"
                             className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
                             name="address"
@@ -75,7 +63,6 @@ const Checkout = () => {
                           <label htmlFor="phone">Phone</label>
                           <input
                             onChange={inputHandle}
-                            value={state.phone}
                             type="text"
                             className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
                             name="phone"
@@ -87,7 +74,6 @@ const Checkout = () => {
                           <label htmlFor="post">Post</label>
                           <input
                             onChange={inputHandle}
-                            value={state.post}
                             type="text"
                             className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
                             name="post"
@@ -101,7 +87,6 @@ const Checkout = () => {
                           <label htmlFor="province">Province</label>
                           <input
                             onChange={inputHandle}
-                            value={state.province}
                             type="text"
                             className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
                             name="province"
@@ -113,7 +98,6 @@ const Checkout = () => {
                           <label htmlFor="city">City</label>
                           <input
                             onChange={inputHandle}
-                            value={state.city}
                             type="text"
                             className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
                             name="city"
@@ -127,7 +111,6 @@ const Checkout = () => {
                           <label htmlFor="area">Area</label>
                           <input
                             onChange={inputHandle}
-                            value={state.area}
                             type="text"
                             className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
                             name="area"
@@ -139,7 +122,6 @@ const Checkout = () => {
                           <label htmlFor="area">Area</label>
                           <input
                             onChange={inputHandle}
-                            value={state.area}
                             type="text"
                             className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
                             name="area"
@@ -178,248 +160,20 @@ const Checkout = () => {
                   </div>
                 )}
               </div>
-
-              {/* {products.map((p, i) => (
-                <div key={i} className="flex bg-white p-4 flex-col gap-2">
-                  <div className="flex justify-start items-center">
-                    <h2 className="text-md text-slate-600">{p.shopName}</h2>
-                  </div>
-                  {p.products.map((pt, j) => (
-                    <div key={i + 99} className="w-full flex flex-wrap">
-                      <div className="flex sm:w-full gap-2 w-7/12">
-                        <div className="flex gap-2 justify-start items-center">
-                          <img
-                            className="w-[80px] h-[80px]"
-                            src={pt.productInfo.images[0]}
-                            alt="product image"
-                          />
-                          <div className="pr-4 text-slate-600">
-                            <h2 className="text-md">{pt.productInfo.name}</h2>
-                            <span className="text-sm">
-                              Brand : {pt.productInfo.brand}
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="flex justify-end w-5/12 sm:w-full sm:mt-3">
-                        <div className="pl-4 sm:pl-0">
-                          <h2 className="text-lg text-orange-500">
-                            $
-                            {pt.productInfo.price -
-                              Math.floor(
-                                (pt.productInfo.price *
-                                  pt.productInfo.discount) /
-                                  100
-                              )}
-                          </h2>
-                          <p className="line-through">
-                            ${pt.productInfo.price}
-                          </p>
-                          <p>-{pt.productInfo.discount}%</p>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              ))} */}
             </div>
 
-            {/* <div className="flex mt-5 flex-col gap-3">
-              <div className="bg-white p-6 shadow-sm rounded-md">
-                {!res && (
-                  <>
-                    <h2 className="text-slate-600 font-bold pb-3">
-                      Shipping Information
-                    </h2>
-                    <form onSubmit={save}>
-                      <div className="flex flex-col lg:flex-row md:gap-2 w-full gap-5 text-slate-600">
-                        <div className="flex flex-col gap-1 mb-2 w-full">
-                          <label htmlFor="name">Name</label>
-                          <input
-                            onChange={inputHandle}
-                            value={state.name}
-                            type="text"
-                            className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
-                            name="name"
-                            placeholder="name"
-                            id="name"
-                          />
-                        </div>
-                        <div className="flex flex-col gap-1 mb-2 w-full">
-                          <label htmlFor="address">Address</label>
-                          <input
-                            onChange={inputHandle}
-                            value={state.address}
-                            type="text"
-                            className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
-                            name="address"
-                            placeholder="House no / building / strreet /area"
-                            id="address"
-                          />
-                        </div>
-                      </div>
-                      <div className="flex flex-col lg:flex-row md:gap-2 w-full gap-5 text-slate-600">
-                        <div className="flex flex-col gap-1 mb-2 w-full">
-                          <label htmlFor="phone">Phone</label>
-                          <input
-                            onChange={inputHandle}
-                            value={state.phone}
-                            type="text"
-                            className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
-                            name="phone"
-                            placeholder="phone"
-                            id="phone"
-                          />
-                        </div>
-                        <div className="flex flex-col gap-1 mb-2 w-full">
-                          <label htmlFor="post">Post</label>
-                          <input
-                            onChange={inputHandle}
-                            value={state.post}
-                            type="text"
-                            className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
-                            name="post"
-                            placeholder="post"
-                            id="post"
-                          />
-                        </div>
-                      </div>
-                      <div className="flex flex-col lg:flex-row md:gap-2 w-full gap-5 text-slate-600">
-                        <div className="flex flex-col gap-1 mb-2 w-full">
-                          <label htmlFor="province">Province</label>
-                          <input
-                            onChange={inputHandle}
-                            value={state.province}
-                            type="text"
-                            className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
-                            name="province"
-                            placeholder="province"
-                            id="province"
-                          />
-                        </div>
-                        <div className="flex flex-col gap-1 mb-2 w-full">
-                          <label htmlFor="city">City</label>
-                          <input
-                            onChange={inputHandle}
-                            value={state.city}
-                            type="text"
-                            className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
-                            name="city"
-                            placeholder="city"
-                            id="city"
-                          />
-                        </div>
-                      </div>
-                      <div className="flex flex-col lg:flex-row md:gap-2 w-full gap-5 text-slate-600">
-                        <div className="flex flex-col gap-1 mb-2 w-full">
-                          <label htmlFor="area">Area</label>
-                          <input
-                            onChange={inputHandle}
-                            value={state.area}
-                            type="text"
-                            className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
-                            name="area"
-                            placeholder="area"
-                            id="province"
-                          />
-                        </div>
-                        <div className="flex flex-col gap-1 mb-2 w-full">
-                          <label htmlFor="area">Area</label>
-                          <input
-                            onChange={inputHandle}
-                            value={state.area}
-                            type="text"
-                            className="w-full px-3 py-2 border border-slate-400 outline-none focus:border-red-400 rounded-md"
-                            name="area"
-                            placeholder="area"
-                            id="province"
-                          />
-                        </div>
-                      </div>
-                    </form>
-                  </>
-                )}
-                {res && (
-                  <div className="flex flex-col gap-1">
-                    <h2 className="text-slate-600 font-semibold pb-2">
-                      Deliver to {state.name}
-                    </h2>
-                    <p>
-                      <span className="bg-blue-200 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">
-                        Home
-                      </span>
-                      <span className="text-slate-600 text-sm">
-                        {state.address} {state.province} {state.city}{" "}
-                        {state.area}
-                      </span>
-                      <span
-                        onClick={() => setRes(false)}
-                        className="text-indigo-500 cursor-pointer"
-                      >
-                        {" "}
-                        change
-                      </span>
-                    </p>
-                    <p className="text-slate-600 text-sm">
-                      Email to sheikhfarid@gmail.com
-                    </p>
-                  </div>
-                )}
-              </div>
-
-              {products.map((p, i) => (
-                <div key={i} className="flex bg-white p-4 flex-col gap-2">
-                  <div className="flex justify-start items-center">
-                    <h2 className="text-md text-slate-600">{p.shopName}</h2>
-                  </div>
-                  {p.products.map((pt, j) => (
-                    <div key={i + 99} className="w-full flex flex-wrap">
-                      <div className="flex sm:w-full gap-2 w-7/12">
-                        <div className="flex gap-2 justify-start items-center">
-                          <img
-                            className="w-[80px] h-[80px]"
-                            src={pt.productInfo.images[0]}
-                            alt="product image"
-                          />
-                          <div className="pr-4 text-slate-600">
-                            <h2 className="text-md">{pt.productInfo.name}</h2>
-                            <span className="text-sm">
-                              Brand : {pt.productInfo.brand}
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="flex justify-end w-5/12 sm:w-full sm:mt-3">
-                        <div className="pl-4 sm:pl-0">
-                          <h2 className="text-lg text-orange-500">
-                            $
-                            {pt.productInfo.price -
-                              Math.floor(
-                                (pt.productInfo.price *
-                                  pt.productInfo.discount) /
-                                  100
-                              )}
-                          </h2>
-                          <p className="line-through">
-                            ${pt.productInfo.price}
-                          </p>
-                          <p>-{pt.productInfo.discount}%</p>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              ))}
-            </div> */}
             <div className="flex gap-2">
               <div className="flex flex-col gap-1 mt-3 w-full">
-                <button className="px-3 py-[6px] hover:bg-indigo-500 hover:text-white duration-300  hover:shadow-indigo-500/20 hover:shadow-lg bg-transparent border rounded-md border-indigo-600">
+                <button
+                  onClick={() => navigate(-1)}
+                  className="px-3 py-[6px] hover:bg-[#F85606] hover:text-white duration-300  hover:shadow-orange-500/20 hover:shadow-lg bg-transparent border rounded-md border-[#F85606]"
+                >
                   Back to Cart
                 </button>
               </div>
               <div className="flex flex-col gap-1 mt-3 w-full">
-                <button className="px-3 py-[6px] rounded-md border hover:shadow-indigo-500/20 hover:shadow-lg bg-indigo-500 text-white">
-                  Proceed to Checkout
+                <button className="px-3 py-[6px] rounded-md border hover:border-[#F85606] hover:text-black hover:bg-white duration-300 hover:shadow-orange-500/20 hover:shadow-lg bg-[#F85606] text-white">
+                  Proceed to Payment
                 </button>
               </div>
             </div>
@@ -430,29 +184,29 @@ const Checkout = () => {
               <div className="bg-white font-medium p-5 text-slate-600 border border-red-500 border-dashed flex flex-col gap-3">
                 <h2 className="text-xl font-semibold">Order Summary</h2>
                 <div className="flex justify-between items-center">
-                  <span>Items Total({price})</span>
-                  <span>${price}</span>
+                  <span>Items Total()</span>
+                  <span>$</span>
                 </div>
                 <div className="flex justify-between items-center">
                   <span>Discount</span>
-                  <span>${shipping_fee}</span>
+                  <span>$</span>
                 </div>
                 <div className="flex justify-between items-center">
                   <span>Delivery Fee</span>
-                  <span>${shipping_fee}</span>
+                  <span>$</span>
                 </div>
                 <div className="flex justify-between items-center">
                   <span>Shipping Fee</span>
-                  <span>${shipping_fee}</span>
+                  <span>$</span>
                 </div>
 
                 <div className="flex justify-between items-center">
                   <span>Total Payment</span>
-                  <span>${price + shipping_fee}</span>
+                  <span>$</span>
                 </div>
                 <div className="flex justify-between items-center">
                   <span>Total</span>
-                  <span>${price + shipping_fee}</span>
+                  <span>$</span>
                 </div>
                 <button
                   disabled={res ? false : true}
