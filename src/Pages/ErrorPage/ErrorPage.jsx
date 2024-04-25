36d7423f1dee41fb46421c2ebb284892cf5b06df
Ahmdpolash <ahmedpolash732@gmail.com>
2024-04-25 22:00:00 +0600

    error page added

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import "./error.css";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
+  return (
+    <div>
+      <div className="bg-gradient-to-r from-purple-300 to-blue-200">
+        <div className="w-9/12 m-auto py-16 min-h-screen flex items-center justify-center">
+          <div className="bg-white shadow overflow-hidden sm:rounded-lg pb-8">
+            <div className="border-t border-gray-200 text-center pt-8">
+              <h1 className="text-9xl font-bold text-purple-400">404</h1>
+              <h1 className="text-6xl font-medium py-8">
+                oops! Page not found
+              </h1>
+              <p className="text-2xl pb-8 px-12 font-medium">
+                Oops! The page you are looking for does not exist. It might have
+                been moved or deleted.
+              </p>
+              <Link to="/">
+                <button className="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-6 py-3 rounded-md mr-6">
+                  HOME
+                </button>
+              </Link>
 
-    return (
-        <div>
-            <h1>error</h1>
+              <Link to="/contact">
+                <button className="bg-gradient-to-r from-red-400 to-red-500 hover:from-red-500 hover:to-red-500 text-white font-semibold px-6 py-3 rounded-md">
+                  Contact Us
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
