18706366b8154c196a142a10eee3f0a1aeaf2f0c
Ahmdpolash <ahmedpolash732@gmail.com>
2024-04-27 00:11:15 +0600

    loader added

--- src/Pages/Shop/ShopLoader.jsx
+++ src/Pages/Shop/ShopLoader.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import loader from "../../assets/loader/shoploader.json";
+import Lottie from "lottie-react";
 
 const ShopLoader = ({ loading }) => {
   return (
     <div className="">
       {loading ? (
-        <div className="absolute inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50 z-30 text-2xl text-white">
-          Loading...
+        <div className="absolute rounded-lg inset-0 flex items-center justify-center bg-slate-500 bg-opacity-50 z-30 text-2xl text-white">
+          <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-gray-100"></div>
+          {/* <Lottie animationData={loader} loop={true} /> */}
         </div>
       ) : (
         ""
